2f57f6b55b3644f99048953816c45e4866512949
Ivan Akulov <mail@iamakulov.com>
2023-07-09 15:54:32 +0200

    Optimizations

--- notes/src/components/NoteView/index.js
+++ notes/src/components/NoteView/index.js
@@ -1,6 +1,8 @@
 import './index.css'
 import ReactMarkdown from 'react-markdown'
 import gfm from 'remark-gfm'
+import LazyHydrate from 'react-lazy-hydration'
+import { Suspense } from 'react'
 
 const MarkdownBlock = ({ children }) => {
   return (
@@ -25,8 +27,19 @@ export default function NoteView({ text }) {
   return (
     <div className="note-view">
       {paragraphs.map((paragraph, index) => {
-        return <MarkdownBlock key={index}>{paragraph}</MarkdownBlock>
+        return (
+          <LazyHydrate whenVisible key={index}>
+            {/* <Suspense> */}
+            <MarkdownBlock>{paragraph}</MarkdownBlock>
+            {/* </Suspense> */}
+          </LazyHydrate>
+        )
       })}
     </div>
   )
 }
+
+// <Suspense>
+// 1) that part of the app is going to be hydrated concurrently
+// 2) upon an interaction (click, keypress, but not mousemove), that part of the app
+//    will switch to old immediate hydration → and it will handle that click or keypress
